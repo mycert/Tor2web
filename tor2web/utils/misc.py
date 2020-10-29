aa25e49da3160a27de6c74e63ead7adb9e4275a3
Fatah al Farihin <30997999+cikgufatah@users.noreply.github.com>
2020-10-30 01:16:29 +0800

    Post fixes after migrated to python3

--- tor2web/utils/misc.py
+++ tor2web/utils/misc.py
@@ -32,27 +32,7 @@ def listenSSLonExistingFD(reactor, fd, factory, contextFactory):
 
 
 def re_sub(pattern, replacement, string):
-    def _r(m):
-        # Now this is ugly.
-        # Python has a "feature" where unmatched groups return None
-        # then re_sub chokes on this.
-        # see http://bugs.python.org/issue1519638
-
-        # this works around and hooks into the internal of the re module...
-
-        # the match object is replaced with a wrapper that
-        # returns "" instead of None for unmatched groups
-
-        class _m():
-            def __init__(self, m):
-                self.m=m
-                self.string=m.string
-            def group(self, n):
-                return m.group(n) or ""
-
-        return re._expand(pattern, _m(m), replacement)
-
-    return re.sub(pattern, _r, string)
+    return re.sub(pattern, replacement, string)
 
 
 def is_onion(hostname):
